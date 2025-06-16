6a6d8a7db70e0af56d859bf0543c56db6ad046b2
dz33 <derekzhao33@gmail.com>
2025-06-15 22:42:15 -0700

    Add getComputerChoice function to javascript.js

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+  var choice = Math.random(3);
+
+  if (choice == 0) {
+    return "rock";
+  }
+
+  if (choice == 1) {
+    return "paper";
+  }
+
+  if (choice == 2) {
+    return "scissors";
+  }
+}
+
