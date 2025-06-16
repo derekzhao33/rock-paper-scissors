ae69a036de431dececfd796d5c3f685cdbbf6623
dz33 <derekzhao33@gmail.com>
2025-06-15 22:55:11 -0700

    Add playRound function to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+var humanScore = 0;
+var computerScore = 0;
+
 function getComputerChoice() {
   var choice = Math.random(3);
 
@@ -16,4 +19,63 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   return prompt("Enter your choice:");
+}
+
+function displayAsWinner(yourChoice, opponentChoice) {
+  console.log("You win! " + yourChoice + " beats " + opponentChoice)
+}
+
+function displayAsLoser(yourChoice, opponentChoice) {
+  console.log("You lose! " + opponentChoice + " beats " + yourChoice);
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice[0].toUpperCase + humanChoice.slice(1).toLowerCase;
+  computerChoice = computerChoice[0].toUpperCase + computerChoice.slice(1).toLowerCase;
+
+  if (humanChoice == computerChoice) {
+    console.log("Tie! No score added")
+  }
+
+  if (humanChoice == "Rock") {
+    chooseRock(computerChoice);
+  }
+
+  if (humanChoice == "Scissors") {
+    chooseScissors(computerChoice);
+  }
+
+  if (humanChoice == "Paper") {
+    choosePaper(computerChoice);
+  }
+}
+
+function chooseRock(opponentChoice) {
+  if (opponentChoice == "Scissors") {
+    displayAsWinner("Rock", opponentChoice);
+  }
+
+  if (opponentChoice == "Paper") {
+    displayAsLoser("Rock", opponentChoice);
+  }
+}
+
+function chooseScissors(opponentChoice) {
+  if (opponentChoice == "Paper") {
+    displayAsWinner("Scissors", opponentChoice);
+  }
+
+  if (opponentChoice == "Rock") {
+    displayAsLoser("Scissors", opponentChoice);
+  }
+}
+
+function choosePaper(opponentChoice) {
+  if (opponentChoice == "Rock") {
+    displayAsWinner("Paper", opponentChoice);
+  }
+
+  if (opponentChoice == "Scissors") {
+    displayAsLoser("Paper", opponentChoice);
+  }
 }
