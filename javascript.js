14e5959ec2e97f1c9fb0d10fef905821d7c3f849
dz33 <derekzhao33@gmail.com>
2025-06-16 23:14:25 -0700

    Add playRound function to javascript.ks

--- javascript.js
+++ javascript.js
@@ -1,93 +1,103 @@
-var humanScore = 0;
-var computerScore = 0;
+function playGame() {
+  var humanScore = 0;
+  var computerScore = 0;
 
-function getComputerChoice() {
-  var choice = Math.floor(Math.random(3) * 3);
-  console.log(choice);
+  
+  function getComputerChoice() {
+    var choice = Math.floor(Math.random(3) * 3);
+    console.log(choice);
 
-  if (choice == 0) {
-    return "Rock";
+    if (choice == 0) {
+      return "Rock";
+    }
+
+    if (choice == 1) {
+      return "Paper";
+    }
+
+    if (choice == 2) {
+      return "Scissors";
+    }
   }
 
-  if (choice == 1) {
-    return "Paper";
+  function getHumanChoice() {
+    return prompt("Enter your choice:");
   }
 
-  if (choice == 2) {
-    return "Scissors";
+  function displayAsWinner(yourChoice, opponentChoice) {
+    console.log("You win! " + yourChoice + " beats " + opponentChoice)
   }
+
+  function displayAsLoser(yourChoice, opponentChoice) {
+    console.log("You lose! " + opponentChoice + " beats " + yourChoice);
+  }
+
+  function chooseRock(opponentChoice) {
+    if (opponentChoice == "Scissors") {
+      displayAsWinner("Rock", opponentChoice);
+      humanScore++;
+    }
+
+    if (opponentChoice == "Paper") {
+      displayAsLoser("Rock", opponentChoice);
+      computerScore++;
+    }
+  }
+
+  function chooseScissors(opponentChoice) {
+    if (opponentChoice == "Paper") {
+      displayAsWinner("Scissors", opponentChoice);
+      humanScore++;
+    }
+
+    if (opponentChoice == "Rock") {
+      displayAsLoser("Scissors", opponentChoice);
+      computerScore++;
+    }
+  }
+
+  function choosePaper(opponentChoice) {
+    if (opponentChoice == "Rock") {
+      displayAsWinner("Paper", opponentChoice);
+      humanScore++;
+    }
+
+    if (opponentChoice == "Scissors") {
+      displayAsLoser("Paper", opponentChoice);
+      computerScore++;
+    }
+  }
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+    if (humanChoice == computerChoice) {
+      console.log("Tie! No score added");
+    }
+
+    if (humanChoice == "Rock") {
+      chooseRock(computerChoice);
+    }
+
+    if (humanChoice == "Scissors") {
+      chooseScissors(computerChoice);
+    }
+
+    if (humanChoice == "Paper") {
+      choosePaper(computerChoice);
+    }
+
+    console.log("You: " + humanScore + ", Computer: " + computerScore)
+  }
+
+  for (let i = 0; i < 5; i++) {
+    
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection)
+  }
+
 }
 
-function getHumanChoice() {
-  return prompt("Enter your choice:");
-}
-
-function displayAsWinner(yourChoice, opponentChoice) {
-  console.log("You win! " + yourChoice + " beats " + opponentChoice)
-}
-
-function displayAsLoser(yourChoice, opponentChoice) {
-  console.log("You lose! " + opponentChoice + " beats " + yourChoice);
-}
-
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
-
-  if (humanChoice == computerChoice) {
-    console.log("Tie! No score added")
-  }
-
-  if (humanChoice == "Rock") {
-    chooseRock(computerChoice);
-  }
-
-  if (humanChoice == "Scissors") {
-    chooseScissors(computerChoice);
-  }
-
-  if (humanChoice == "Paper") {
-    choosePaper(computerChoice);
-  }
-}
-
-function chooseRock(opponentChoice) {
-  if (opponentChoice == "Scissors") {
-    displayAsWinner("Rock", opponentChoice);
-    humanScore++;
-  }
-
-  if (opponentChoice == "Paper") {
-    displayAsLoser("Rock", opponentChoice);
-    computerScore++;
-  }
-}
-
-function chooseScissors(opponentChoice) {
-  if (opponentChoice == "Paper") {
-    displayAsWinner("Scissors", opponentChoice);
-    humanScore++;
-  }
-
-  if (opponentChoice == "Rock") {
-    displayAsLoser("Scissors", opponentChoice);
-    computerScore++;
-  }
-}
-
-function choosePaper(opponentChoice) {
-  if (opponentChoice == "Rock") {
-    displayAsWinner("Paper", opponentChoice);
-    humanScore++;
-  }
-
-  if (opponentChoice == "Scissors") {
-    displayAsLoser("Paper", opponentChoice);
-    computerScore++;
-  }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-
-playRound(humanSelection, computerSelection);
+playGame();
