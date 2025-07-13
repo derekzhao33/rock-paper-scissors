e075ed8f88cf1f57431466ed2c7257b3347b342c
dz33 <derekzhao33@gmail.com>
2025-07-12 23:26:56 -0700

    Add event listeners to buttons

--- javascript.js
+++ javascript.js
@@ -1,94 +1,105 @@
-function playGame() {
-  var humanScore = 0;
-  var computerScore = 0;
+var humanScore = 0;
+var computerScore = 0;
 
-  
-  function getComputerChoice() {
-    var choice = Math.floor(Math.random(3) * 3);
-    console.log(choice);
 
-    if (choice == 0) {
-      return "Rock";
-    }
+function getComputerChoice() {
+  var choice = Math.floor(Math.random(3) * 3);
+  console.log(choice);
 
-    if (choice == 1) {
-      return "Paper";
-    }
-
-    if (choice == 2) {
-      return "Scissors";
-    }
+  if (choice == 0) {
+    return "Rock";
   }
 
-  function getHumanChoice() {
-    return prompt("Enter your choice:");
+  if (choice == 1) {
+    return "Paper";
   }
 
-  function displayAsWinner(yourChoice, opponentChoice) {
-    console.log("You win! " + yourChoice + " beats " + opponentChoice)
-  }
-
-  function displayAsLoser(yourChoice, opponentChoice) {
-    console.log("You lose! " + opponentChoice + " beats " + yourChoice);
-  }
-
-  function chooseRock(opponentChoice) {
-    if (opponentChoice == "Scissors") {
-      displayAsWinner("Rock", opponentChoice);
-      humanScore++;
-    }
-
-    if (opponentChoice == "Paper") {
-      displayAsLoser("Rock", opponentChoice);
-      computerScore++;
-    }
-  }
-
-  function chooseScissors(opponentChoice) {
-    if (opponentChoice == "Paper") {
-      displayAsWinner("Scissors", opponentChoice);
-      humanScore++;
-    }
-
-    if (opponentChoice == "Rock") {
-      displayAsLoser("Scissors", opponentChoice);
-      computerScore++;
-    }
-  }
-
-  function choosePaper(opponentChoice) {
-    if (opponentChoice == "Rock") {
-      displayAsWinner("Paper", opponentChoice);
-      humanScore++;
-    }
-
-    if (opponentChoice == "Scissors") {
-      displayAsLoser("Paper", opponentChoice);
-      computerScore++;
-    }
-  }
-
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
-
-    if (humanChoice == computerChoice) {
-      console.log("Tie! No score added");
-    }
-
-    if (humanChoice == "Rock") {
-      chooseRock(computerChoice);
-    }
-
-    if (humanChoice == "Scissors") {
-      chooseScissors(computerChoice);
-    }
-
-    if (humanChoice == "Paper") {
-      choosePaper(computerChoice);
-    }
-
-    console.log("You: " + humanScore + ", Computer: " + computerScore)
+  if (choice == 2) {
+    return "Scissors";
   }
 }
 
-playGame();
+function getHumanChoice() {
+  return prompt("Enter your choice:");
+}
+
+function displayAsWinner(yourChoice, opponentChoice) {
+  console.log("You win! " + yourChoice + " beats " + opponentChoice)
+}
+
+function displayAsLoser(yourChoice, opponentChoice) {
+  console.log("You lose! " + opponentChoice + " beats " + yourChoice);
+}
+
+function chooseRock(opponentChoice) {
+  if (opponentChoice == "Scissors") {
+    displayAsWinner("Rock", opponentChoice);
+    humanScore++;
+  }
+
+  if (opponentChoice == "Paper") {
+    displayAsLoser("Rock", opponentChoice);
+    computerScore++;
+  }
+}
+
+function chooseScissors(opponentChoice) {
+  if (opponentChoice == "Paper") {
+    displayAsWinner("Scissors", opponentChoice);
+    humanScore++;
+  }
+
+  if (opponentChoice == "Rock") {
+    displayAsLoser("Scissors", opponentChoice);
+    computerScore++;
+  }
+}
+
+function choosePaper(opponentChoice) {
+  if (opponentChoice == "Rock") {
+    displayAsWinner("Paper", opponentChoice);
+    humanScore++;
+  }
+
+  if (opponentChoice == "Scissors") {
+    displayAsLoser("Paper", opponentChoice);
+    computerScore++;
+  }
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+  if (humanChoice == computerChoice) {
+    console.log("Tie! No score added");
+  }
+
+  if (humanChoice == "Rock") {
+    chooseRock(computerChoice);
+  }
+
+  if (humanChoice == "Scissors") {
+    chooseScissors(computerChoice);
+  }
+
+  if (humanChoice == "Paper") {
+    choosePaper(computerChoice);
+  }
+
+  console.log("You: " + humanScore + ", Computer: " + computerScore)
+}
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+rock.addEventListener("click", () => {
+  playRound("Rock", getComputerChoice());
+});
+paper.addEventListener("click", () => {
+  playRound("Paper", getComputerChoice());
+});
+scissors.addEventListener("click", () => {
+  playRound("Scissors", getComputerChoice());
+});
+
